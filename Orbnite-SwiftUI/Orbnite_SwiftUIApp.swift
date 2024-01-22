584910da0a8ee31177625dfb8a8ccbe180703b08
Febin Puthalath <febz@Febins-MacBook-Pro-2.local>
2024-01-22 14:24:01 +0530

    initial commit

---
+++ Orbnite-SwiftUI/Orbnite_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orbnite_SwiftUIApp.swift
+//  Orbnite-SwiftUI
+//
+//  Created by Febin Puthalath on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Orbnite_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
